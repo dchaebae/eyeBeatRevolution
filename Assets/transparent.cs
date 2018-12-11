010fca903170864569a7fa9c31cd5bcedf085144
dchaebae <danielkchae@gmail.com>
2018-12-10 23:49:49 -0500

    bubbles added

---
+++ Assets/transparent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transparent : MonoBehaviour
+{
+    public FoveInterfaceBase foveInterface;
+
+    private Material mat;
+    private Collider surface;
+    // Use this for initialization
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mat.color = new Color(1.0f, 1.0f, 1.0f, 0.7f);
+
+        surface = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (foveInterface.Gazecast(surface))
+        {
+            mat.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        else
+        {
+            mat.color = new Color(1.0f, 1.0f, 1.0f, 0.7f);
+        }
+    }
+}
